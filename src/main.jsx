64ebb197176499636404b381df09cfa07411414f
halioti2 <153960879+halioti2@users.noreply.github.com>
2025-04-23 17:51:07 -0400

    CP 2A Supabase Client Initialization

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
